324b950db090b12d2882863f0f6c52cb14ff295a
Alienah <aidalbla@gmail.com>
2018-04-20 10:18:44 +0200

    Initial structure for testing

---
+++ spec/appSpec.js
@@ -0,0 +1,10 @@
+describe("javascript allonge", function(){
+  describe("value types", function(){
+    it("4 is equal to 2+2", function(){
+    
+      expect(2+2).toEqual(4);
+    });
+  });
+  
+  
+});
